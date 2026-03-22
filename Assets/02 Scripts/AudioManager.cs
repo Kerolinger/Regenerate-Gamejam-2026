eae701239cbebc82693b20b6830ce014ef5d4220
kerolinger <kerolinger@gmail.com>
2026-03-22 11:40:14 +0100

    audiomanager fixes

--- Assets/02 Scripts/AudioManager.cs
+++ Assets/02 Scripts/AudioManager.cs
@@ -35,10 +35,6 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void Start()
-    {
-        Play("main theme");
-    }
     //____________________________________________________________________
 
     public void Play ( string name)
