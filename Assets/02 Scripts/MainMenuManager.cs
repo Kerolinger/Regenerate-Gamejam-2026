82038ab9fe8cf03bf775ed32b0da5fb16538182b
kerolinger <kerolinger@gmail.com>
2026-03-22 11:14:47 +0100

    ++

--- Assets/02 Scripts/MainMenuManager.cs
+++ Assets/02 Scripts/MainMenuManager.cs
@@ -6,18 +6,29 @@ public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject m_credits;
 
+    private void Start()
+    {
+        AudioManager.instance.Play("wholesomeTheme");
+        AudioManager.instance.Play("ambienceStageThree");
+    }
     public void BTN_StartGame()
     {
         SceneManager.LoadScene(1);
+        AudioManager.instance.Play("buttonConfirm");
     }
 
     public void BTN_ToggleCredits()
     {
         m_credits.SetActive(!m_credits.activeSelf);
+
+        if (m_credits.activeSelf)
+            AudioManager.instance.Play("buttonConfirm");
+        else
+            AudioManager.instance.Play("buttonBack");
     }
 
-    public void BTN_QuitGame()
-    {
-        Application.Quit();
-    }
+    //public void BTN_QuitGame()
+    //{
+    //    Application.Quit();
+    //}
 }
